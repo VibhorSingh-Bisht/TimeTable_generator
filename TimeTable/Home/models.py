d836fc6fc9353fcdb5067691a77776d6e131667a
Vibhor <vibhor0711@gmail.com>
2024-03-13 20:15:38 +0530

    first commit

---
+++ TimeTable/Home/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class teacher_data(models.Model):
+    teacher_name = models.CharField(max_length = 35)
+    teacher_desig = models.CharField(max_length = 15)
+    subjects = models.CharField(max_length = 150)
+    
+
+
---
+++ TimeTable/Home/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    """peoples = [
+        {"Name":"Vibhor Singh","age":21},
+        {"Name":"Abhijeet","age":22},
+        {"Name":"ACP Parduman","age":21},
+        {"Name":"Daya","age":21}
+    ]"""
+    return render(request, "Add Teacher.html")#, context={'peoples':peoples})
+
+def add_course(request):
+    return render(request,"Add Teacher.html")
+
+def add_dept(request):
+    pass
+
+def add_teacher(request):
+    if request.method == 'POST':
+        data = request.POST
+        
+    return render(request, "Add Teacher.html")
+
+def index(request):
+    pass
+
+def dashboard(request):
+    pass
+
+def add_timing(request):
+    pass

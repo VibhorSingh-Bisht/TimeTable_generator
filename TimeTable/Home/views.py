85d5fadb7cfe4bbfc79e4055a04a254c21179fac
Vibhor <vibhor0711@gmail.com>
2024-03-14 10:38:49 +0530

    first commit

--- TimeTable/Home/views.py
+++ TimeTable/Home/views.py
@@ -1,14 +1,9 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 def home(request):
-    """peoples = [
-        {"Name":"Vibhor Singh","age":21},
-        {"Name":"Abhijeet","age":22},
-        {"Name":"ACP Parduman","age":21},
-        {"Name":"Daya","age":21}
-    ]"""
     return render(request, "Add Teacher.html")#, context={'peoples':peoples})
 
 def add_course(request):
@@ -21,6 +16,15 @@ def add_teacher(request):
     if request.method == 'POST':
         data = request.POST
         teacher_name = data.get('teacher_name')
+        teacher_desig = data.get('teacher_desig')
+        subjects = data.get('subjects')
+        print(teacher_name, teacher_desig,subjects)
+        teacher_data.objects.create(
+            teacher_name  = teacher_name,
+            teacher_desig = teacher_desig,
+            subjects = subjects
+        )
+
     return render(request, "Add Teacher.html")
 
 def index(request):
--- TimeTable/TimeTable/urls.py
+++ TimeTable/TimeTable/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from Home.views import home, add_course
+from Home.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',home,name='home'),
-    path('',add_course,name='add_course')
+    path('add_teacher',add_teacher,name='add_teacher')
 ]

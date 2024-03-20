be9a5c1e7ad50ebb071b6bd559f7ccbe5cb62d94
Vibhor <vibhor0711@gmail.com>
2024-03-20 12:33:24 +0530

    first commit

--- TimeTable/Home/models.py
+++ TimeTable/Home/models.py
@@ -12,3 +12,14 @@ class course_data(models.Model):
     course_name = models.CharField(max_length=25)
     course_c = models.PositiveSmallIntegerField()
     course_subs = models.CharField(max_length=200)
+
+class timing_data(models.Model):
+    course_name = models.CharField(max_length=25)
+    course_c = models.PositiveSmallIntegerField()
+    course_subs = models.CharField(max_length=200)
+
+
+class department_data(models.Model):
+    course_name = models.CharField(max_length=25)
+    course_c = models.PositiveSmallIntegerField()
+    course_subs = models.CharField(max_length=200)

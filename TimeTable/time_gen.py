c7f16ead1f496db5a8db0d36c0369069d25442cd
VibhorSingh-Bisht <vibhor0711bp@gmail.com>
2024-09-04 17:20:40 +0530

    first commit

--- TimeTable/time_gen.py
+++ TimeTable/time_gen.py
@@ -1,3 +1,5 @@
+## Generates time according to the organization
+
 def main(iterations=4, start_hour=8, start_minute=50,increment_time = 50, special_break = 10):
     l = []
     l.append(f'{start_hour:02d}:{start_minute:02d}')
